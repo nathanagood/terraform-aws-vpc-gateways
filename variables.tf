10fd3fa9614dfc184fcaf221bb579da0c0cafa6f
indirakalagara <indira.kalagara@in.ibm.com>
2022-05-06 10:13:19 +0530

    Update Github actions (#11)
    * added output igw ids as list
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated github actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * Update variables.tf
    
    * Update verify.yaml
    
    * Rename verify.yaml to verify-push.yaml
    
    * Update and rename verify-push.yaml to verify.yaml
    
    * Delete verify-pr.yaml
    
    * updated workflow actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated workflow actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated workflow actions
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated verify.yaml
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    Co-authored-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "name" {
   type        = string
   description = "The name of the IGW instance"
-  default     = "" 
+  default     = ""
 }
 
 variable "name_prefix"{
@@ -15,12 +15,6 @@ variable "vpc_name" {
   default = ""
 }
 
-# variable "vpc_id" {
-#   type = string
-#   description =  "The ID of the VPC instance"  
-# }
-
-
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group where the VPC is deployed. On AWS this value becomes a tag."
@@ -31,11 +25,3 @@ variable "provision" {
   description = "Flag to determine whether to provision Internet gateway or not. Defautl set to true"
   default = true
 }
-
-# variable "_count" {
-#   type = number
-#   description = "Number of resources to be provisioned"
-#   default = 1
-  
-# }
-
